d60237252cc4bb82ce55009f0115b65b92cb029b
Poonam Mohata <poonammohata2@gmail.com>
2022-10-03 16:53:09 +0530

    adds js code in new file

---
+++ index.js
@@ -0,0 +1,13 @@
+mapboxgl.accessToken = YOUR_MAPBOX_ACCESS_TOKEN;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+        projection: 'globe' // display the map as a 3D globe
+    });
+
+    map.on('style.load', () => {
+        map.setFog({}); // Set the default atmosphere style
+    });
